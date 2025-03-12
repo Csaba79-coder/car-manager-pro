0bf5ee3b8003220536e7085f5f8b4b2e59c312a0
Csaba Vadász <csabavadasz79@gmail.com>
2025-03-12 12:59:24 +0100

    Initial commit

---
+++ src/main/java/com/csaba79coder/carmanagepro/CarManagerProApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.carmanagepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagerProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarManagerProApplication.class, args);
+    }
+
+}
